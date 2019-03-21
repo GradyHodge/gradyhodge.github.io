2b02a1286fb1bbc9afd6b54ce50bc998d1c34553
Use your GitHub username <gradyhodge.github.io>
2019-03-21 14:19:01 -0500

    update to redo wk 4 header and responsive menu

--- GitHub_Website/assignments/lesson-10/weather-api.js
+++ GitHub_Website/assignments/lesson-10/weather-api.js
@@ -6,4 +6,12 @@ weatherRequest.send();
 weatherRequest.onload = function () {
 	let weatherData = JSON.parse(weatherRequest.responseText);
 	console.log(weatherData);
+
+	document.getElementById('cc-temp')	.innerHTML = weatherData.main.temp
+	let icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon +".png"
+	let desc = weatherData.weather.description;
+
+	document.getElementById('cc-img').setAttribute('src', icon)
+	document.getElementById('cc-img').setAttribute('alt', desc)
 }
+ 
