6c5e844261ffde157e5b18c4363705520a04c9a6
Use your GitHub username <gradyhodge.github.io>
2019-04-04 00:00:56 -0500

    temple Inn Project adding responsive nav bar

--- GitHub_Website/assignments/lesson-12/term-project/scripts/main.js
+++ GitHub_Website/assignments/lesson-12/term-project/scripts/main.js
@@ -1,2 +1,10 @@
-// copyright update
+// copyright update - done inline
 
+//navbar toggle
+
+const toggleButton = document.getElementsByClassName('toggleButton')[0];
+const navbarLinks = document.getElementsByClassName('navbarLinks')[0];
+
+toggleButton.addEventListener('click', () => {
+	navbarLinks.classList.toggle('active')
+})
