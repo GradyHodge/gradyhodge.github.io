43392ce1641737053f0b4ad414080de295d74dab
Use your GitHub username <gradyhodge.github.io>
2019-04-04 13:42:44 -0500

    w3schools responsive nav with dropdown menu added

---
+++ GitHub_Website/assignments/lesson-12/term-project/scripts/temple-script.js
@@ -0,0 +1,11 @@
+/*Responsive Nav Bar with dropdown experiment from w3c
+ * 
+ */
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
